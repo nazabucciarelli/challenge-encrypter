53dfdc63474e2c2a205d3861607706e4ffdf33d9
Nazareno Bucciarelli <n.bucciarelli@itecriocuarto.org.ar>
2023-04-18 16:26:08 -0300

    finalizado primer version del challenge

---
+++ js/encrypter.js
@@ -0,0 +1,76 @@
+let encryptButton = document.getElementById("button-encrypt");
+let desencryptButton = document.getElementById("button-desencrypt");
+let textarea = document.querySelector("textarea");
+let notfound = document.getElementById("container-notfound");
+let result = document.getElementById("result");
+let resultText = document.getElementById("result-text");
+let copyButton = document.getElementById("button-copy");
+
+function encrypt(){
+    let text = textarea.value;
+    let encryptedText = "";
+    for(let i = 0; i < text.length;i++){
+        if(text[i] == 'a') {
+            encryptedText = encryptedText.concat("a","ai");
+        }
+        else if(text[i] == 'e'){
+            encryptedText = encryptedText.concat("e","enter");
+        }
+        else if(text[i] == 'i'){
+            encryptedText =  encryptedText.concat("i","imes");
+        }
+        else if(text[i] == 'o'){
+            encryptedText = encryptedText.concat("o","ober");
+        }
+        else if(text[i] == 'u'){
+            encryptedText = encryptedText.concat("u","ufat");
+        } else{
+            encryptedText = encryptedText.concat(text[i]);
+        }
+    }
+    if(encryptedText.length > 0){
+        showResult(encryptedText);    
+    }
+}
+
+function desencrypt(){
+    let text = textarea.value;
+    let desencryptedText = "";
+    for(let i = 0;i < text.length;i++){
+        if(text.substring(i,i+5) == "enter"){
+            desencryptedText = desencryptedText.concat("e");
+            i += 4;
+        } else if(text.substring(i,i+4) == "imes"){
+            desencryptedText = desencryptedText.concat("i");
+            i += 3;
+        } else if(text.substring(i,i+2) == "ai"){
+            desencryptedText = desencryptedText.concat("a");
+            i += 1;
+        } else if(text.substring(i,i+4) == "ober"){
+            desencryptedText = desencryptedText.concat("o");
+            i += 3;
+        } else if(text.substring(i,i+4) == "ufat"){
+            desencryptedText = desencryptedText.concat("u");
+            i += 3;
+        } else{
+            desencryptedText = desencryptedText.concat(text[i]);
+        }
+    }
+    if(desencryptedText.length > 0){
+        showResult(desencryptedText);
+    }
+}
+
+function showResult(msg){
+    notfound.classList.add("hidden");
+    result.classList.remove("hidden");
+    resultText.innerHTML = msg;
+}
+
+function copy(){
+    navigator.clipboard.writeText(resultText.innerHTML);
+}
+
+encryptButton.onclick =  encrypt;
+desencryptButton.onclick = desencrypt;
+copyButton.onclick = copy;
